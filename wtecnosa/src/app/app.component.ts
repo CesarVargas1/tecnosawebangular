aa0fc02bda57ff6efa0cb6c0173af9b686f04183
CesarVargas1 <cavargas769@misena.edu.co>
2023-02-01 08:52:32 -0500

    se actualiza npm node divide html y css al header

--- wtecnosa/src/app/app.component.ts
+++ wtecnosa/src/app/app.component.ts
@@ -1,9 +1,15 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+  styleUrls: ['./app.component.css' ],
+
+
+
 })
 export class AppComponent {
   title = 'wtecnosa';
